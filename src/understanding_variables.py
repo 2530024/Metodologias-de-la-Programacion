301301b33e6268d533279691234985d97df8cf5b
Tu Nombre <tuemail@ejemplo.com>
2025-10-27 09:47:23 -0600

    feat: first class

---
+++ src/understanding_variables.py
@@ -0,0 +1,23 @@
+message = "This is my first variable!"
+another_message = "Hello, World!"
+
+print(message)
+print(another_message)
+
+print(message, another_message, message, another_message)
+print(another_message, message)
+"""
+Los nombres de las variables en Python deben nombrarse solo con:
+
+    - Las letras, numeros y guion bajos (espacios)
+    -deben comenzar con una letra o con guieno bajo, pero NO con numero:
+        ejemplo correcto : mensaje_1 ( :) )
+        ejemplo incorrecto : 1_mensaje ( :( )
+    -no utilizar palabras reservadas de Python para nombrar variables o archivos
+    -no utilizar palabras reservadas de Python para nombrar variables o archivos
+    -nombres cortos ser cortos, pero descriptivos
+    -ser en ingles
+    -nombres de las variables en minusculas,
+"""
+charly_message = "Charly es un buen profe"
+print(charly_message)
