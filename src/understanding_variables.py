d96c5e4c2a62482cea08ef0c547a2252827b84ff
Tu Nombre <tuemail@ejemplo.com>
2025-10-29 09:29:01 -0600

    feat: traceback

--- src/understanding_variables.py
+++ src/understanding_variables.py
@@ -19,5 +19,20 @@ Los nombres de las variables en Python deben nombrarse solo con:
     -ser en ingles
     -nombres de las variables en minusculas,
 """
+
 charly_message = "Charly es un buen profe"
+print (charly_message)
 print(charly_message)
+
+"""
+Traceback: Es un registro de donde el interprete 
+        tuvo problemas para ejecutar el codigo
+
+    Ejemplo 
+    Traceback (most recent call last):
+        File "C:/Users/selen/Proyectos/Metodologias-de-la-Programacion/src/understanding_variables.py", line 25, in <module>
+        print(charly_mesage)
+    NameError: name 'charly_mesage' is not defined
+
+    NameError: Significa que la variable no tiene valor de una variable asignada
+"""
